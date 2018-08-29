f01d344b30a5dabc723149700aef4fb6ecc4a704
Adam Sanghera <adamsanghera@gmail.com>
2018-08-29 00:53:28 -0400

    adding a generic gen-proto script

--- pkg/subscribe/subscribepb/compile.sh
+++
@@ -1,6 +0,0 @@
-# Remove the dead weeds, if they exist
-rm -rf *.pb.go > /dev/null 2>&1
-
-# Compile the files
-protoc --go_out=plugins=grpc:$GOPATH/src/github.com/adamsanghera/go-websub/pkg/subscribe/subscribepb \
-       ./service.proto
